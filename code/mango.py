e4570df7984e48c52f2733eb7306cddfa04898e3
Manassanan Vongprai <2779884v@student.gla.ac.uk>
2024-09-18 22:10:39 +0100

    almost done with first part of kasiski, banished friedman

--- code/mango.py
+++ code/mango.py
@@ -11,3 +11,17 @@
     wrote this.
 '''
 
+def friedman(text):
+    cleanup = ''.join([char for char in text if char.isalpha()])
+    length = len(cleanup)
+    #count =  Counter(cleanup)
+    count_list = [list(i) for i in count.items()]
+    nmrt=0
+    for x in range(len(count_list)):
+        if count_list[x][0].isalpha():
+            nmrt += count_list[x][1] * (count_list[x][1]-1)
+    ioc = nmrt/ (length * (length-1))
+    denom = (.065 - ioc) + (length * (ioc - .0385))
+    denom2 = (length-1) * ioc - (0.038*length) + 0.065
+    #key_length = math.ceil(.0265 * length / denom2)
+    #print(key_length)
--- code/vig_bruteforce.py
+++ code/vig_bruteforce.py
@@ -1,6 +1,6 @@
 '''
 todo:
-- kasiski (find groups of repeated letters)
+/ kasiski (find groups of repeated letters)
     > find gcd (gcd is key length)
 - there is duplicate clean up code. refactor l8r
 - group letters and then frequency analysis
@@ -11,16 +11,17 @@ todo:
 from vigenere import vig_decrypt
 from collections import Counter   # counter is used during frequency analysis
 import math
+import numpy
 
 LETTER_FREQ = 'etaoinsrhdlucmfywgpbvkxqjz' # https://pi.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html
-substring_dict = {}
+
+
+
+pot_key_val = 0
 
 # kasiski part
-def get_gcd(list):
-    # function that finds the gcd of given list of numbers
-    return math.gcd(*list)
-
 def repeated_sequence(text,length=3):
+    substring_dict = {}
     cleaned_text = ''.join([char for char in text if char.isalpha()])
     for i in range(len(cleaned_text)-length+1):
         temp = cleaned_text[i:i+length] 
@@ -28,14 +29,37 @@ def repeated_sequence(text,length=3):
             substring_dict[temp].append(i)
         else:
             substring_dict[temp] = [i]
-    print(substring_dict)
+    return substring_dict
 
-def kasiski():
+def subtraction(dict):
+    difference_list = []
+    for key,value in dict.items():
+        if(len(value)>1):
+            diff = numpy.diff(value)
+            difference_list.extend(diff)
+    return difference_list
+
+def get_gcd(list):
+    # function that finds the gcd of given list of numbers
+    candidate_dic = {}
+    for x in range(len(list)):
+        for y in range(x+1,len(list)):
+            num = math.gcd(list[x],list[x+1])
+            if num in candidate_dic:
+                candidate_dic[num] += 1
+            else:
+                candidate_dic[num] = 1
+    print(candidate_dic)
+
+def kasiski(text):
     # kasiski finds repeated letters, take notes of starting point, subtract, then get_gcd() after
     # will have a list of results from subtraction
+    dict = repeated_sequence(text)
+    dif_list = subtraction(dict)
+    print(dif_list)
+    get_gcd(dif_list)
 
-    pass
-
+    
 
 # frequency analysis part
 def count():
@@ -43,22 +67,11 @@ def count():
     pass
 
 # friedman
-def friedman(text):
-    cleanup = ''.join([char for char in text if char.isalpha()])
-    length = len(cleanup)
-    count =  Counter(cleanup)
-    count_list = [list(i) for i in count.items()]
-    nmrt=0
-    for x in range(len(count_list)):
-        if count_list[x][0].isalpha():
-            nmrt += count_list[x][1] * (count_list[x][1]-1)
-    ioc = nmrt/ (length * (length-1))
-    denom = (.065 - ioc) + (length * (ioc - .0385))
-    denom2 = (length-1) * ioc - (0.038*length) + 0.065
-    key_length = math.ceil(.0265 * length / denom2)
-    print(key_length)
+# note to self : ignore this and work on kasiski
+# banished friedman into oblivion
 
 
-
-friedman("yhtl xaj tje aytt kioe nmv fvln ic jpvv? wjai bjd zt hetj mibe? qkpw. j sve. cns rie srgazsq hlrv lxif hvln rxeit? yoy dxb zmrncgt rp mfvg oc dsod sqmtmoe khct lyt yfut lxdflzng? dxb uhv tjojeit ff jib ujty sqmtmoe vlue rpvsy yqug qpuc apd icbr powr wcbrk aragr?")
-repeated_sequence("yhtl xaj tje aytt kioe nmv fvln ic jpvv? wjai bjd zt hetj mibe? qkpw. j sve. cns rie srgazsq hlrv lxif hvln rxeit? yoy dxb zmrncgt rp mfvg oc dsod sqmtmoe khct lyt yfut lxdflzng? dxb uhv tjojeit ff jib ujty sqmtmoe vlue rpvsy yqug qpuc apd icbr powr wcbrk aragr?")
+kasiski("yhtl xaj tje aytt kioe nmv fvln ic jpvv? wjai bjd zt hetj mibe? qkpw. j sve. cns rie srgazsq hlrv lxif hvln rxeit? yoy dxb zmrncgt rp mfvg oc dsod sqmtmoe khct lyt yfut lxdflzng? dxb uhv tjojeit ff jib ujty sqmtmoe vlue rpvsy yqug qpuc apd icbr powr wcbrk aragr?")
+kasiski("Ic psp vvrsdfzv nmok mo jvqh cmfi kt pv acscj? Pvgvyjj W usi’x. Z tbcc midjasim lzr, hyi rep ms deyi vasicolzsu wizp ijoc. Rja zy’g aynx hzwvx, gmbj W’d avmknbx jjv jtavxcmel hyeo’w ejjvv xsdnbx fvgb. N azwn xyj krc ci wnzciy xyj gzpzrtj. Bfa, dx’j oijx hi—vrdkc, gsjy. W usi’x bscn acek yc us rmkm hyi ntrhs yi giwy pvldru. Nh’j pdov yfpmik kt vfpy seyc jshikmwek olry krw iimjf iivpcd hyimi.")
+kasiski("A tsizs A fplx ngr ehq dwrdiav gf jog bzae oztq eiiebk iy mk pwao. Ttm gnp wtw didtqvk, wso gvvecsfifdd, wtw keps ym lhp wmg A wtst ggu nogtv. Ie’s xqce T cdmstpd fpas aednwce iyiye zf kwm, pteomv tzgqbzec fdwe tse yweeyte ezece kwm wprq idmzsf ezae I zmwdpd. Ncl tse fzmts ie, ggu’ce zwl htm, mzw yzu? Kwm npvqz oece. U swea hatviyg av lo ehua adpa an ohz yac uoflp jw, wso U eass yac oece, ncl ie’s vckt l stivoh—a riftlsk. Ifd yo yiltpr two mfct Q oayt ub lo me dmsl, T kzwo dpeb lgwy ttil I’x iz tgvp wubz szmqwfe hha lgedn’f mpidt.")
+kasiski("k gdr hofsgbjkqs nhgn nmv tflf mt wpu coxes kf")
