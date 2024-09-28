1473efe04418d663df73aa58ff8a11f9b109a48f
Manassanan Vongprai <2779884v@student.gla.ac.uk>
2024-09-28 01:28:48 +0100

    program guesses unknown key correctly in most cases !! :D happy

--- code/vig_bruteforce.py
+++ code/vig_bruteforce.py
@@ -11,12 +11,12 @@ BIGRAMs = ['th','he','in','er','an','re','on','at','en',
            'ou','io','le','ve','co','me','de','hi','ri'
            'ro','ic','ne','ea','ra','ce']
 
-expected_frequencies = {
-    'E': 12.49, 'T': 9.28, 'A': 8.04, 'O': 7.64, 'I': 7.57, 'N': 7.23,
-    'S': 6.51, 'R': 6.28, 'H': 5.05, 'L': 4.07, 'D': 3.82, 'C': 3.34,
-    'U': 2.73, 'M': 2.51, 'F': 2.40, 'P': 2.14, 'G': 1.87, 'W': 1.68,
-    'Y': 1.66, 'B': 1.48, 'V': 1.05, 'K': 0.54, 'X': 0.23, 'J': 0.16,
-    'Q': 0.12, 'Z': 0.09
+EXPECTED_FREQ = {
+    'e': 0.1249, 't': 0.0928, 'a': 0.0804, 'o': 0.0764, 'i': 0.0757, 'n': 0.0723,
+    's': 0.0651, 'r': 0.0628, 'h': 0.0505, 'l': 0.0407, 'd': 0.0382, 'c': 0.0334,
+    'u': 0.0273, 'm': 0.0251, 'f': 0.024, 'p': 0.0214, 'g': 0.0187, 'w': 0.0168,
+    'y': 0.0166, 'b': 0.0148, 'v': 0.0105, 'k': 0.0054, 'x': 0.0023, 'j': 0.0016,
+    'q': 0.0012, 'z': 0.0009
 }
 
 # kasiski part (useless without repeats in ciphertext)
@@ -66,18 +66,28 @@ def letter_groups(keys,text):
 def freq_analysis(groupings):
     decrypted_grps = []
     pot_key = ''
+    
     for g in groupings:
+        smol_chi = float('inf')
         for i in range(0,26):
-            pot_letter = chr((i % 26) + ord('a'))
-            chi(caesar_decrypt(g,i)) # pass to chi
+            chi_num = chi(caesar_decrypt(g,i)) # pass to chi
         # chi spits back a number
         # keep track of smallest number
+            if smol_chi > chi_num:
+                smol_chi = chi_num
+                pot_letter = chr((i % 26) + ord('a'))
+                
         # wahetever letter created smallest number is probably key
+        pot_key += pot_letter
+        print(pot_key)
         
 def chi(decrypted_text):
     c = Counter(decrypted_text)
     length = len(decrypted_text)
-    print(c,length)
+    chisq = 0
+    for letter,count in c.items():
+        chisq += ((count-EXPECTED_FREQ[letter]*length)**2) / EXPECTED_FREQ[letter]
+    return chisq
 
 
 def kasiski(text):
@@ -102,8 +112,8 @@ def count():
 
 #kasiski("gvzmyfhhebpwauvgvdnqvbohojcmlnvhcixxwntrrwialvzdtibtohouihaxzwiddbqjrbrzvtonzgidfqjndrbosgrzdvobbvpvnqddsfzvnnbtdgxbfvnmrwitrraddcgcabvnqfsongjfsatdnsgmvnnvhracacomonbotrnhrecucplnictaqrtvr")
 kasiski("yhtl xaj tje aytt kioe nmv fvln ic jpvv? wjai bjd zt hetj mibe? qkpw. j sve. cns rie srgazsq hlrv lxif hvln rxeit? yoy dxb zmrncgt rp mfvg oc dsod sqmtmoe khct lyt yfut lxdflzng? dxb uhv tjojeit ff jib ujty sqmtmoe vlue rpvsy yqug qpuc apd icbr powr wcbrk aragr?")
-#kasiski("Ic psp vvrsdfzv nmok mo jvqh cmfi kt pv acscj? Pvgvyjj W usi’x. Z tbcc midjasim lzr, hyi rep ms deyi vasicolzsu wizp ijoc. Rja zy’g aynx hzwvx, gmbj W’d avmknbx jjv jtavxcmel hyeo’w ejjvv xsdnbx fvgb. N azwn xyj krc ci wnzciy xyj gzpzrtj. Bfa, dx’j oijx hi—vrdkc, gsjy. W usi’x bscn acek yc us rmkm hyi ntrhs yi giwy pvldru. Nh’j pdov yfpmik kt vfpy seyc jshikmwek olry krw iimjf iivpcd hyimi.")
-#kasiski("A tsizs A fplx ngr ehq dwrdiav gf jog bzae oztq eiiebk iy mk pwao. Ttm gnp wtw didtqvk, wso gvvecsfifdd, wtw keps ym lhp wmg A wtst ggu nogtv. Ie’s xqce T cdmstpd fpas aednwce iyiye zf kwm, pteomv tzgqbzec fdwe tse yweeyte ezece kwm wprq idmzsf ezae I zmwdpd. Ncl tse fzmts ie, ggu’ce zwl htm, mzw yzu? Kwm npvqz oece. U swea hatviyg av lo ehua adpa an ohz yac uoflp jw, wso U eass yac oece, ncl ie’s vckt l stivoh—a riftlsk. Ifd yo yiltpr two mfct Q oayt ub lo me dmsl, T kzwo dpeb lgwy ttil I’x iz tgvp wubz szmqwfe hha lgedn’f mpidt.")
+kasiski("ic psp vvrsdfzv nmok mo jvqh cmfi kt pv acscj? pvgvyjj w usi’x. z tbcc midjasim lzr, hyi rep ms deyi vasicolzsu wizp ijoc. rja zy’g aynx hzwvx, gmbj w’d avmknbx jjv jtavxcmel hyeo’w ejjvv xsdnbx fvgb. n azwn xyj krc ci wnzciy xyj gzpzrtj. bfa, dx’j oijx hi—vrdkc, gsjy. w usi’x bscn acek yc us rmkm hyi ntrhs yi giwy pvldru. nh’j pdov yfpmik kt vfpy seyc jshikmwek olry krw iimjf iivpcd hyimi.")
+#kasiski("a tsizs A fplx ngr ehq dwrdiav gf jog bzae oztq eiiebk iy mk pwao. Ttm gnp wtw didtqvk, wso gvvecsfifdd, wtw keps ym lhp wmg A wtst ggu nogtv. Ie’s xqce T cdmstpd fpas aednwce iyiye zf kwm, pteomv tzgqbzec fdwe tse yweeyte ezece kwm wprq idmzsf ezae I zmwdpd. Ncl tse fzmts ie, ggu’ce zwl htm, mzw yzu? Kwm npvqz oece. U swea hatviyg av lo ehua adpa an ohz yac uoflp jw, wso U eass yac oece, ncl ie’s vckt l stivoh—a riftlsk. Ifd yo yiltpr two mfct Q oayt ub lo me dmsl, T kzwo dpeb lgwy ttil I’x iz tgvp wubz szmqwfe hha lgedn’f mpidt.")
 #friedman("k gdr hofsgbjkqs nhgn nmv tflf mt wpu coxes kf")
 kasiski("altd hlbe tg lrncmwxpo kpxs evl ztrsuicp qptspf. ivplyprr th pw clhoic pozc. :-)")
 
